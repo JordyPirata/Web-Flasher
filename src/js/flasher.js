0f7c95e67cd47ba1a82a6b0366fb73e9f27259c0
JordyPirata <jordi77alva@gmail.com>
2025-02-21 22:54:15 -0600

    JavaScript device handling

--- src/js/flasher.js
+++ src/js/flasher.js
@@ -1,3 +1,4 @@
+
 const deviceinfo = [{
     'name': 'oneplus-enchilada',
     'nicename': 'OnePlus 6',
@@ -26,33 +27,34 @@ const deviceinfo = [{
   }
 ];
 
+async function onConnectDevice(device) {
+
+}
+
 document.addEventListener("DOMContentLoaded", async function() {
 
   if (navigator.usb === undefined) {
 
     const connectedDiv = document.getElementById('ConnectedDev');
     const SupportedDiv = document.getElementById('SupportedDev');
+    const paragraph = document.getElementById("paragraph");
 
+    paragraph.style.display = 'none';
     SupportedDiv.style.display = 'none';
     connectedDiv.style.display = 'none';
     console.error("No webusb support");
     return;
   }
-  else 
-  {
+  else {
     const div = document.getElementById('notsupported');
 
     div.style.display = 'none';
   }
-    
   
   let devices = await navigator.usb.getDevices();
   for (let i = 0; i < devices.length; i++) {
     onConnectDevice(devices[i]);
   }
-  
-  navigator.usb.addEventListener('connect', onConnect);
-  navigator.usb.addEventListener('disconnect', onDisonnect);
   
   const requestDeviceButton = document.getElementById('request-device');
   requestDeviceButton.addEventListener('click', async function(event) {
@@ -74,4 +76,19 @@ document.addEventListener("DOMContentLoaded", async function() {
       onConnectDevice(device);
     }
   });
+
+  const suppTable = document.getElementById('supporteddevices');
+  for (let i = 0; i < deviceinfo.length; i++) {
+    const di = deviceinfo[i];
+    const row = document.createElement('TR');
+    const td_name = document.createElement('TD');
+    const name_link = document.createElement('A');
+    name_link.innerText = di['nicename'];
+    td_name.appendChild(name_link);
+    row.appendChild(td_name);
+    const td_codename = document.createElement('TD');
+    td_codename.innerText = di['name'];
+    row.appendChild(td_codename);
+    suppTable.appendChild(row);
+  }
 }); 
