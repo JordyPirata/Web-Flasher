3983d992e76d8adc29ad206d60d53dd7d36c8ede
JordyPirata <jordi77alva@gmail.com>
2025-02-27 09:23:37 -0600

    Refactor project structure: move to dist and remove autogenerated files

--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,9 @@ const path = require('path');
 module.exports = {
   entry: './src/index.js',
   mode: 'development',
+  devtool: "source-map",
   output: {
     filename: 'main.js',
-    path: path.resolve(__dirname, 'public'),
+    path: path.resolve(__dirname, 'dist'),
   },
 };
